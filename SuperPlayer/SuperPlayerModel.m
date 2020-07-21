81af48673346e9d9456ce36891ed806069502d4a
Tony <tony.yu@uhomes.com>
2020-07-21 14:47:40 +0800

    升级AFNetworing库

--- SuperPlayer/SuperPlayerModel.m
+++ SuperPlayer/SuperPlayerModel.m
@@ -110,114 +110,115 @@ NSNotificationName kSuperPlayerModelFail = @"kSuperPlayerModelFail";
     
     __weak SuperPlayerModel *weakSelf = self;
     __weak SuperPlayerView  *weakPlayerView = playerView;
-    [manager GET:url parameters:params progress:nil
-                                success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                                    
-                                    __strong SuperPlayerModel *self = weakSelf;
-                                    
-                                    NSString *masterUrl = J2Str([responseObject valueForKeyPath:@"videoInfo.masterPlayList.url"]);
-                                    //    masterUrl = nil;
-                                    if (masterUrl.length > 0) {
-                                        // 1. 如果有master url，优先用这个
-                                        self.videoURL = masterUrl;
-                                    } else {
-                                        NSString *mainDefinition = J2Str([responseObject valueForKeyPath:@"playerInfo.defaultVideoClassification"]);
-                                        
-                                        
-                                        NSArray *videoClassification = J2Array([responseObject valueForKeyPath:@"playerInfo.videoClassification"]);
-                                        NSArray *transcodeList = J2Array([responseObject valueForKeyPath:@"videoInfo.transcodeList"]);
-                                        
-                                        NSMutableArray<SuperPlayerUrl *> *result = [NSMutableArray new];
-                                        
-                                        // 2. 如果有转码的清晰度，用转码流
-                                        for (NSDictionary *transcode in transcodeList) {
-                                            SuperPlayerUrl *subModel = [SuperPlayerUrl new];
-                                            subModel.url = J2Str(transcode[@"url"]);
-                                            NSNumber *theDefinition = J2Num(transcode[@"definition"]);
-                                            
-                                            
-                                            for (NSDictionary *definition in videoClassification) {
-                                                for (NSObject *definition2 in J2Array([definition valueForKeyPath:@"definitionList"])) {
-                                                    
-                                                    if ([definition2 isEqual:theDefinition]) {
-                                                        subModel.title = J2Str([definition valueForKeyPath:@"name"]);
-                                                        NSString *definitionId = J2Str([definition valueForKeyPath:@"id"]);
-                                                        // 初始播放清晰度
-                                                        if ([definitionId isEqualToString:mainDefinition]) {
-                                                            if (![self.videoURL containsString:@".mp4"])
-                                                                self.videoURL = subModel.url;
-                                                        }
-                                                        break;
-                                                    }
-                                                }
-                                            }
-                                            // 同一个清晰度可能存在多个转码格式，这里只保留一种格式，且优先mp4类型
-                                            for (SuperPlayerUrl *item in result) {
-                                                if ([item.title isEqual:subModel.title]) {
-                                                    if (![item.url containsString:@".mp4"]) {
-                                                        item.url = subModel.url;
-                                                    }
-                                                    subModel = nil;
-                                                    break;
-                                                }
-                                            }
-                                            
-                                            if (subModel) {
-                                                [result addObject:subModel];
-                                            }
-                                        }
-                                        self.multiVideoURLs = result;
-                                    }
-                                    // 3. 以上都没有，用原始地址
-                                    if (self.videoURL == nil) {
-                                        NSString *source = J2Str([responseObject valueForKeyPath:@"videoInfo.sourceVideo.url"]);
-                                        self.videoURL = source;
-                                    }
-                                    
-                                    NSArray *imageSprites = J2Array([responseObject valueForKeyPath:@"imageSpriteInfo.imageSpriteList"]);
-                                    if (imageSprites.count > 0) {
-                                        //                 id imageSpriteObj = imageSprites[0];
-                                        id imageSpriteObj = imageSprites.lastObject;
-                                        NSString *vtt = J2Str([imageSpriteObj valueForKeyPath:@"webVttUrl"]);
-                                        NSArray *imgUrls = J2Array([imageSpriteObj valueForKeyPath:@"imageUrls"]);
-                                        NSMutableArray *imgUrlArray = @[].mutableCopy;
-                                        for (NSString *url in imgUrls) {
-                                            NSURL *nsurl = [NSURL URLWithString:url];
-                                            if (nsurl) {
-                                                [imgUrlArray addObject:nsurl];
-                                            }
-                                        }
-                                        
-                                        TXImageSprite *imageSprite = [[TXImageSprite alloc] init];
-                                        [imageSprite setVTTUrl:[NSURL URLWithString:vtt] imageUrls:imgUrlArray];
-                                        weakPlayerView.imageSprite = imageSprite;
-                                    } else {
-                                        weakPlayerView.imageSprite = nil;
-                                    }
-                                    
-                                    NSArray *keyFrameDescList = J2Array([responseObject valueForKeyPath:@"keyFrameDescInfo.keyFrameDescList"]);
-                                    if (keyFrameDescList.count > 0) {
-                                        weakPlayerView.keyFrameDescList = keyFrameDescList;
-                                    } else {
-                                        weakPlayerView.keyFrameDescList = nil;
-                                    }
-                                    
-                                    
-                                    [[NSNotificationCenter defaultCenter] postNotificationName:kSuperPlayerModelReady
-                                                                                        object:self
-                                                                                      userInfo:@{
-                                                                                                 @"message": @"success"
-                                                                                                 }];
-                                    
-                                } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                                    
-                                    [[NSNotificationCenter defaultCenter] postNotificationName:kSuperPlayerModelFail
-                                                                                        object:self
-                                                                                      userInfo:@{
-                                                                                                 @"error": error,
-                                                                                                 @"message": @"请求失败"                                          }];
-                                }];
-    
+    [manager GET:url parameters:params headers:nil progress:nil
+         success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+             
+             __strong SuperPlayerModel *self = weakSelf;
+             
+             NSString *masterUrl = J2Str([responseObject valueForKeyPath:@"videoInfo.masterPlayList.url"]);
+             //    masterUrl = nil;
+             if (masterUrl.length > 0) {
+                 // 1. 如果有master url，优先用这个
+                 self.videoURL = masterUrl;
+             } else {
+                 NSString *mainDefinition = J2Str([responseObject valueForKeyPath:@"playerInfo.defaultVideoClassification"]);
+                 
+                 
+                 NSArray *videoClassification = J2Array([responseObject valueForKeyPath:@"playerInfo.videoClassification"]);
+                 NSArray *transcodeList = J2Array([responseObject valueForKeyPath:@"videoInfo.transcodeList"]);
+                 
+                 NSMutableArray<SuperPlayerUrl *> *result = [NSMutableArray new];
+                 
+                 // 2. 如果有转码的清晰度，用转码流
+                 for (NSDictionary *transcode in transcodeList) {
+                     SuperPlayerUrl *subModel = [SuperPlayerUrl new];
+                     subModel.url = J2Str(transcode[@"url"]);
+                     NSNumber *theDefinition = J2Num(transcode[@"definition"]);
+                     
+                     
+                     for (NSDictionary *definition in videoClassification) {
+                         for (NSObject *definition2 in J2Array([definition valueForKeyPath:@"definitionList"])) {
+                             
+                             if ([definition2 isEqual:theDefinition]) {
+                                 subModel.title = J2Str([definition valueForKeyPath:@"name"]);
+                                 NSString *definitionId = J2Str([definition valueForKeyPath:@"id"]);
+                                 // 初始播放清晰度
+                                 if ([definitionId isEqualToString:mainDefinition]) {
+                                     if (![self.videoURL containsString:@".mp4"])
+                                         self.videoURL = subModel.url;
+                                 }
+                                 break;
+                             }
+                         }
+                     }
+                     // 同一个清晰度可能存在多个转码格式，这里只保留一种格式，且优先mp4类型
+                     for (SuperPlayerUrl *item in result) {
+                         if ([item.title isEqual:subModel.title]) {
+                             if (![item.url containsString:@".mp4"]) {
+                                 item.url = subModel.url;
+                             }
+                             subModel = nil;
+                             break;
+                         }
+                     }
+                     
+                     if (subModel) {
+                         [result addObject:subModel];
+                     }
+                 }
+                 self.multiVideoURLs = result;
+             }
+             // 3. 以上都没有，用原始地址
+             if (self.videoURL == nil) {
+                 NSString *source = J2Str([responseObject valueForKeyPath:@"videoInfo.sourceVideo.url"]);
+                 self.videoURL = source;
+             }
+             
+             NSArray *imageSprites = J2Array([responseObject valueForKeyPath:@"imageSpriteInfo.imageSpriteList"]);
+             if (imageSprites.count > 0) {
+                 //                 id imageSpriteObj = imageSprites[0];
+                 id imageSpriteObj = imageSprites.lastObject;
+                 NSString *vtt = J2Str([imageSpriteObj valueForKeyPath:@"webVttUrl"]);
+                 NSArray *imgUrls = J2Array([imageSpriteObj valueForKeyPath:@"imageUrls"]);
+                 NSMutableArray *imgUrlArray = @[].mutableCopy;
+                 for (NSString *url in imgUrls) {
+                     NSURL *nsurl = [NSURL URLWithString:url];
+                     if (nsurl) {
+                         [imgUrlArray addObject:nsurl];
+                     }
+                 }
+                 
+                 TXImageSprite *imageSprite = [[TXImageSprite alloc] init];
+                 [imageSprite setVTTUrl:[NSURL URLWithString:vtt] imageUrls:imgUrlArray];
+                 weakPlayerView.imageSprite = imageSprite;
+             } else {
+                 weakPlayerView.imageSprite = nil;
+             }
+             
+             NSArray *keyFrameDescList = J2Array([responseObject valueForKeyPath:@"keyFrameDescInfo.keyFrameDescList"]);
+             if (keyFrameDescList.count > 0) {
+                 weakPlayerView.keyFrameDescList = keyFrameDescList;
+             } else {
+                 weakPlayerView.keyFrameDescList = nil;
+             }
+             
+             
+             [[NSNotificationCenter defaultCenter] postNotificationName:kSuperPlayerModelReady
+                                                                 object:self
+                                                               userInfo:@{
+                                                                          @"message": @"success"
+                                                                          }];
+             
+         }
+         failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+             
+             [[NSNotificationCenter defaultCenter] postNotificationName:kSuperPlayerModelFail
+                                                                 object:self
+                                                               userInfo:@{
+                                                                          @"error": error,
+                                                                          @"message": @"请求失败"
+                                                               }];
+         }];
 }
 
 /*
