16b766cac4fa365922ce53a89d88ca72d0ff922d
zhuang.ma <zhuang.ma@uhomes.com>
2023-06-05 17:54:41 +0800

    增加字幕视图文件

---
+++ SuperPlayer/Views/SuperPlayerSubtitlesView/SuperPlayerSubtitlesView.h
@@ -0,0 +1,16 @@
+//
+//  SuperPlayerSubtitlesView.h
+//  AFNetworking
+//
+//  Created by 马壮 on 2023/6/5.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SuperPlayerSubtitlesView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SuperPlayer/Views/SuperPlayerSubtitlesView/SuperPlayerSubtitlesView.m
@@ -0,0 +1,20 @@
+//
+//  SuperPlayerSubtitlesView.m
+//  AFNetworking
+//
+//  Created by 马壮 on 2023/6/5.
+//
+
+#import "SuperPlayerSubtitlesView.h"
+
+@implementation SuperPlayerSubtitlesView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
